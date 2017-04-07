a6ceec33b7fc9c620f90aa6b4fa09545292d4751
Bryam Rodriguez <brodriguezaybar@gmail.com>
2017-04-07 16:12:22 -0500

    Add Singleton example

---
+++ objects-creation/singleton.js
@@ -0,0 +1,34 @@
+class Singleton {
+  constructor() {
+    if (!Singleton.instance) {
+      Singleton.instance = this.init();
+    }
+    return Singleton.instance;
+  }
+
+  init() {
+    function privateMethod() {
+      console.log( "I am private" );
+    }
+
+    let privateRandomNumber = Math.round(Math.random()*1000);
+
+    return {
+      publicMethod: function () {
+        privateMethod();
+        console.log( "The public can see me!" );
+      },
+      publicProperty: "I am also public",
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+    }
+  }
+}
+
+var singleA = new Singleton();
+var singleB = new Singleton();
+
+console.log(`singleA random has a value of ${singleA.getRandomNumber()}`);
+console.log(`singleB random has a value of ${singleB.getRandomNumber()}`);
+console.log("same? " + (singleA.getRandomNumber() === singleB.getRandomNumber()) );
